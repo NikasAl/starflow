2ce5a94ce7168c894019a9bb27cc36fcaf589fad
Nikas <nikita_avdonin@mail.ru>
2026-04-18 17:55:41 +0400

    remove unneeded

---
+++ starflow-3d/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.starflow.game',
+  appName: 'starflow-3d',
+  webDir: 'dist'
+};
+
+export default config;
